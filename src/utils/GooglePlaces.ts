602cd8f651fa11081211e6cd49e102be36aa10d4
vynious <shawnthiah@gmail.com>
2023-06-14 00:50:38 +0800

    some updates

--- src/utils/GooglePlaces.ts
+++ src/utils/GooglePlaces.ts
@@ -4,13 +4,13 @@ import Logging from "./Logging";
 
 dotenv.config();
 
-
+const API_KEY = process.env.GOOGLE_PLACES_API_KEY
 
 // get coordinates from frontend -> pass to backend -> backend processes it
 export const nearbyFoodOptions = async (location: Coordinates) => {
     try {
         const {long, lat} = location
-        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&type=restaurant&key=AIzaSyD7CTQ_WJ-RGRiuWiH1WVcrIaNaCnF5Bco`;
+        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&type=restaurant&key=${API_KEY}`;
         const data = await fetch(url)
         if (data) {
             return data.json()
